d377f1daf70de8c92fcce4574f3e8212ad3c51bc
Gustavo Nascimento <ngustavo80@hotmail.com>
2021-10-22 07:07:26 -0300

    first commit

---
+++ backend/src/app.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import express from 'express';
+
+import { router } from "./routes";
+
+const app = express();
+app.use(express.json());
+
+app.use(router);
+
+app.listen(4000, () => console.log('Server running on port 4000.'));
+
+app.get("/github", (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
+
+app.get("/signin/callback", (req, res) => {
+  const { code } = req.query;
+
+  return res.json(code);
+})
